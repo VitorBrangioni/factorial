30587d087d5a85e66d6a16bc669e6b761136ef6a
vitor.brangioni <vitor.brangioni@SQEST125.squadra.com.br>
2017-03-29 20:16:38 -0300

    Começo da implementação do metodo calculateFactorial.

--- src/factorial/Factorial.java
+++ src/factorial/Factorial.java
@@ -1,5 +1,24 @@
 package factorial;
 
-public class Factorial {
+import java.math.BigInteger;
 
+public class Factorial {
+	
+	private static BigInteger result = new BigInteger("1");
+	
+	private Factorial() {
+	}
+
+	// @TODO
+	public static String calculateFactorial(Integer n) {
+		BigInteger nBigInteger = new BigInteger(n.toString());
+		
+		if (n == 0) {
+			return result.toString();
+		}
+		result = result.multiply(nBigInteger);
+		
+		
+		return null;
+	}
 }
